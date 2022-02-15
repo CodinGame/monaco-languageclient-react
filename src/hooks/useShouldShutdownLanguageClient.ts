713da507f8c4cc2abe0b3cff27a32c577b97360b
Loïc Mangeonjean <loic@codingame.com>
2022-02-15 11:14:13 +0100

    feat: Add some hooks

---
+++ src/hooks/useShouldShutdownLanguageClient.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+
+export default function useShouldShutdownLanguageClient (userActive: boolean, delay: number): boolean {
+  const [paused, setPaused] = useState(false)
+  useEffect(() => {
+    setPaused(false)
+    if (!userActive) {
+      const timeout = setTimeout(() => {
+        setPaused(true)
+      }, delay)
+      return () => {
+        clearTimeout(timeout)
+      }
+    }
+    return () => {}
+  }, [userActive, delay])
+  return paused
+}
